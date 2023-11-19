0d2356c748360974b7628766cb20d540be158306
Dave Reed <dave256@mac.com>
2023-11-19 14:16:31 -0500

    Package.swift swift-parsing
    platforms, dependencies

--- Package.swift
+++ Package.swift
@@ -5,17 +5,27 @@ import PackageDescription
 
 let package = Package(
     name: "Graphics2D",
+    platforms: [
+        .iOS(.v16),
+        .macOS(.v13)
+    ],
     products: [
         // Products define the executables and libraries a package produces, making them visible to other packages.
         .library(
             name: "Graphics2D",
             targets: ["Graphics2D"]),
     ],
+    dependencies: [
+        // Dependencies declare other packages that this package depends on.
+        .package(url: "https://github.com/pointfreeco/swift-parsing.git", from:
+                    "0.13.0")
+    ],
     targets: [
         // Targets are the basic building blocks of a package, defining a module or a test suite.
         // Targets can depend on other targets in this package and products from dependencies.
         .target(
-            name: "Graphics2D"),
+            name: "Graphics2D",
+            dependencies: [.product(name: "Parsing", package: "swift-parsing")]),
         .testTarget(
             name: "Graphics2DTests",
             dependencies: ["Graphics2D"]),
